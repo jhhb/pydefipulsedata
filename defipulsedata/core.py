b3d1cd33c92ee9d75bf71159a6c8a3e88b341f18
admin <admin@admin-c.local>
2021-03-30 19:50:28 -0400

    We can run python files now.

---
+++ defipulsedata/core.py
@@ -0,0 +1,8 @@
+# This is where the bulk of logic will go, and we will want a base client and clients for each
+# individual API.
+class Core:
+    def __init__(self):
+        print('test')
+
+
+print('hello world')
