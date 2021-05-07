866912a6758b6c1a868442b0331d6c8a04540959
admin <admin@admin-c.local>
2021-05-07 17:53:59 -0400

    Remove junk files

--- defipulsedata/core.py
+++
@@ -1,8 +0,0 @@
-# This is where the bulk of logic will go, and we will want a base client and clients for each
-# individual API.
-class Core:
-    def __init__(self):
-        print('test')
-
-
-print('hello world')
