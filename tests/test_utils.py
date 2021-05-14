6b7f919a939fc21e3d31d9e5aa47a139542145b3
admin <admin@admin-c.local>
2021-05-14 11:21:57 -0400

    Use correct type. Add missing kwarg

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,9 +3,9 @@ import responses
 
 from defipulsedata import utils
 
-global EMPTY_BLOB
+global EMPTY_DICT
 # TODO: JB - Check on the actual return type.
-EMPTY_BLOB = '{}'
+EMPTY_DICT = {}
 
 
 class TestWrapper(unittest.TestCase):
@@ -15,21 +15,20 @@ class TestWrapper(unittest.TestCase):
         expected_url = 'https://data-api.defipulse.com/api/v1/egs/api/ethgasAPI.json?api-key=mock-key'
 
         responses.add(responses.GET, expected_url,
-                      json=EMPTY_BLOB, status=500)
+                      json=EMPTY_DICT, status=500)
         self.assertRaises(ValueError, utils.get_request, expected_url)
 
         responses.reset()
         responses.add(responses.GET, expected_url,
-                      json=EMPTY_BLOB, status=400)
+                      json=EMPTY_DICT, status=400)
         self.assertRaises(ValueError, utils.get_request, expected_url)
 
         responses.reset()
         responses.add(responses.GET, expected_url,
-                      EMPTY_BLOB, status=200)
+                      json=EMPTY_DICT, status=200)
         utils.get_request(expected_url)
         self.assertEqual(responses.calls[0].request.url, expected_url)
 
-    @responses.activate
     def test_validate_allowed_params(self):
         empty_params = {}
         params = {'foo': 'bar'}
