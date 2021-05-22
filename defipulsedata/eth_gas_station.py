81af8ce68500e4c11ad0e2b6e4eda556c0be35c9
admin <admin@admin-c.local>
2021-05-22 18:43:32 -0400

    Use consistent instance varialbes in egs client

--- defipulsedata/eth_gas_station.py
+++ defipulsedata/eth_gas_station.py
@@ -1,3 +1,5 @@
+from urllib import parse
+
 from .utils import get_request
 
 
@@ -5,21 +7,19 @@ class EthGasStation:
     __API_URL_BASE = 'https://data-api.defipulse.com/api/v1/egs/api'
 
     def __init__(self, *, api_key):
-        self.api_key = api_key
         self.api_base_url = self.__API_URL_BASE
+        self.base_params = {'api-key': api_key}
 
     def get_gas_price(self):
         "Returns the current recommended fast, standard and safe low gas prices on the Ethereum network, along with the current block and wait times for each 'speed'."
 
-        api_url = '{0}/ethgasAPI.json?api-key={1}'.format(
-            self.api_base_url, self.api_key
-        )
+        encoded_params = parse.urlencode(self.base_params)
+        api_url = '{0}/ethgasAPI.json?{1}'.format(self.api_base_url, encoded_params)
         return get_request(api_url)
 
     def get_prediction_table(self):
         "Returns estimated confirmation times for a range of gas prices from the EGS Prediction Page."
 
-        api_url = '{0}/predictTable.json?api-key={1}'.format(
-            self.api_base_url, self.api_key
-        )
+        encoded_params = parse.urlencode(self.base_params)
+        api_url = '{0}/predictTable.json?{1}'.format(self.api_base_url, encoded_params)
         return get_request(api_url)
